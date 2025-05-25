b4b2c5a405571966c4fdde788f4760e371703538
HenriF197 <hnrqff1948@outlook.com>
2025-05-25 11:00:24 -0300

    Update xadrez.c
    atualizando com uma nova peça o cavalo utilizando loops aninhados.

--- xadrez.c
+++ xadrez.c
@@ -1,35 +1,53 @@
- #include <stdio.h>
+#include <stdio.h>
 int main () {
 
-int bispo = 1, rainha;
+int bispo = 0, rainha = 0, cavalo = 1;
 
     
     //movendo a torre 5 casas para direita
-    for (int torre = 0; torre < 1; torre++) 
+    for (int torre = 0; torre < 5; torre++) 
     {
         printf ("Torre para Direita!\n"); //imprimindo direção de movimento
+        
     }
+    printf ("\n");
 
     //movendo bispo para diagonal
     do {
-        printf ("%cBispo para cima!\n", bispo);
-        printf ("%cBispo para direita!\n", bispo);
+        printf ("Bispo para cima!\n", bispo);
+        printf ("Bispo para direita!\n", bispo);
        
         bispo++;
     
     }while (bispo < 1);
 
+    printf ("\n");
+
     //rainha se move em qualquer direção
-    while (rainha < 1)
+    while (rainha < 1 )
     {
-        printf ("%cRainha para cima!\n", rainha);
-        printf ("%cRainha para direita!\n", rainha);
-        printf ("%cRainha para baixo!\n", rainha);
-        printf ("%cRainha para esquerda!\n", rainha);
+        printf ("Rainha para cima!\n", rainha);
+        printf ("Rainha para direita!\n", rainha);
+        printf ("Rainha para baixo!\n", rainha);
+        printf ("Rainha para esquerda!\n", rainha);
         rainha++;
+
+        printf ("\n");
+    
+    }
+
+    while (cavalo--)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            printf ("Cavalo baixo!\n");
+        }
+        
+        printf ("Cavalo esquerda!\n");
     }
     
     
+   
     
     
     
